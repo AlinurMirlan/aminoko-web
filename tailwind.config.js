93edb060c3f0533dfc9a331ca0477020d241a1ee
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-03-22 10:58:15 +0600

    feat: :art: add react hook form and tailwindcss and their dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logo: "rgb(var(--color-logo) / <alpha-value>)",
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        tertiary: "rgb(var(--color-tertiary) / <alpha-value>)",
+        error: "rgb(var(--color-error) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+        outline: "rgb(var(--color-outline) / <alpha-value>)",
+        "primary-container":
+          "rgb(var(--color-primary-container) / <alpha-value>)",
+        "secondary-container":
+          "rgb(var(--color-secondary-container) / <alpha-value>)",
+        "tertiary-container":
+          "rgb(var(--color-tertiary-container) / <alpha-value>)",
+        "error-container": "rgb(var(--color-error-container) / <alpha-value>)",
+        "surface-variant": "rgb(var(--color-surface-variant) / <alpha-value>)",
+        "on-primary": "rgb(var(--color-on-primary) / <alpha-value>)",
+        "on-secondary": "rgb(var(--color-on-secondary) / <alpha-value>)",
+        "on-tertiary": "rgb(var(--color-on-tertiary) / <alpha-value>)",
+        "on-error": "rgb(var(--color-on-error) / <alpha-value>)",
+        "on-background": "rgb(var(--color-on-background) / <alpha-value>)",
+        "on-primary-container":
+          "rgb(var(--color-on-primary-container) / <alpha-value>)",
+        "on-secondary-container":
+          "rgb(var(--color-on-secondary-container) / <alpha-value>)",
+        "on-tertiary-container":
+          "rgb(var(--color-on-tertiary-container) / <alpha-value>)",
+        "on-error-container":
+          "rgb(var(--color-on-error-container) / <alpha-value>)",
+        "on-surface-variant":
+          "rgb(var(--color-on-surface-variant) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
