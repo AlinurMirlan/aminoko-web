b77fca1676ada52a6180df7d4a213f0e4ca49e7a
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-04-10 23:23:16 +0600

    feat: replace fetching instance data to hardcoding it and adhere it to new standards

--- src/views/repeat/Repeat.tsx
+++ src/views/repeat/Repeat.tsx
@@ -1,55 +1,24 @@
-import { useSelector } from "react-redux";
 import { FlashcardBack } from "../../components/flashcard/FlashcardBack";
-import { RootState } from "../../data/store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FlashcardFront } from "../../components/flashcard/FlashcardFront";
 
-type FlashcardData = { front: string; back: string };
-
 export function Repeat() {
-  useEffect(() => {
-    async function fetchFlashcards() {
-      const response = await fetch("../../dummyFlashcardData.json");
-      const data = (await response.json()) as FlashcardData;
-      return data;
-    }
-
-    fetchFlashcards()
-      .then((data) => {
-        setFlashcardData(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const bottomPanelDimensions = useSelector(
-    (state: RootState) => state.dimensions.bottomPanelDimensions
-  );
   const [isFlipped, setIsFlipped] = useState(false);
-  const [flashcardData, setFlashcardData] = useState<FlashcardData>({
-    front: "",
-    back: "",
-  });
 
-  return (
-    <div className="flex flex-col flex-grow">
-      {isFlipped ? (
-        <FlashcardBack
-          className="flex-grow"
-          onClickFlip={() => setIsFlipped(false)}
-        >
-          {flashcardData.back}
-        </FlashcardBack>
-      ) : (
-        <FlashcardFront
-          className="flex-grow"
-          onClickFlip={() => setIsFlipped(true)}
-        >
-          {flashcardData.front}
-        </FlashcardFront>
-      )}
-      <div style={{ height: bottomPanelDimensions.height }}></div>
-    </div>
+  return isFlipped ? (
+    <FlashcardBack
+      className="flex-grow"
+      onClickFlip={() => setIsFlipped(false)}
+    >
+      Efface - принижать. To efface oneself is not the easiest of duties which
+      the teacher can undertake.
+    </FlashcardBack>
+  ) : (
+    <FlashcardFront
+      className="flex-grow"
+      onClickFlip={() => setIsFlipped(true)}
+    >
+      Make oneself appear insignificant or inconspicuous.
+    </FlashcardFront>
   );
 }
