dad54000ff39abb10d4c256201d6f708703b55ea
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-04-10 23:20:42 +0600

    style: change spacing between input fields

--- src/views/authentication/FormSignIn.tsx
+++ src/views/authentication/FormSignIn.tsx
@@ -26,35 +26,38 @@ export function FormSignIn() {
   return (
     <div className="text-on-background p-4 w-full">
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <InputForm
-            label="Email Address"
-            id="email"
-            error={errors.email}
-            {...register("email", {
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "Entered value does not match email format",
-              },
-              required: "Email is required",
-            })}
-          />
+        <div className="flex flex-col gap-0.5">
+          <div>
+            <InputForm
+              label="Email Address"
+              id="email"
+              error={errors.email}
+              {...register("email", {
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+                required: "Email is required",
+              })}
+            />
+          </div>
+          <div className="mb-1">
+            <InputForm
+              label="Password"
+              id="password"
+              error={errors.password}
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters",
+                },
+              })}
+            />
+          </div>
         </div>
-        <div className="mb-1">
-          <InputForm
-            label="Password"
-            id="password"
-            error={errors.password}
-            type="password"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must have at least 8 characters",
-              },
-            })}
-          />
-        </div>
+
         <div className="flex gap-4 justify-between">
           <Button type="submit" className="shrink-0" Icon={IconFingerprint}>
             Sign in
--- src/views/authentication/FormSignUp.tsx
+++ src/views/authentication/FormSignUp.tsx
@@ -25,47 +25,49 @@ export function FormSignUp() {
   return (
     <div className="text-on-background p-4 w-full">
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <InputForm
-            label="Email Address"
-            id="email"
-            error={errors.email}
-            {...register("email", {
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "Entered value does not match email format",
-              },
-              required: "Email is required",
-            })}
-          />
-        </div>
-        <div>
-          <InputForm
-            label="Password"
-            id="password"
-            error={errors.password}
-            type="password"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must have at least 8 characters",
-              },
-            })}
-          />
-        </div>
-        <div className="mb-1">
-          <InputForm
-            label="Confirm Password"
-            id="confirmPassword"
-            error={errors.confirmPassword}
-            type="password"
-            {...register("confirmPassword", {
-              required: "Please confirm your password",
-              validate: (value) =>
-                value === watch("password") || "Passwords do not match",
-            })}
-          />
+        <div className="flex flex-col gap-0.5">
+          <div>
+            <InputForm
+              label="Email Address"
+              id="email"
+              error={errors.email}
+              {...register("email", {
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+                required: "Email is required",
+              })}
+            />
+          </div>
+          <div>
+            <InputForm
+              label="Password"
+              id="password"
+              error={errors.password}
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters",
+                },
+              })}
+            />
+          </div>
+          <div className="mb-1">
+            <InputForm
+              label="Confirm Password"
+              id="confirmPassword"
+              error={errors.confirmPassword}
+              type="password"
+              {...register("confirmPassword", {
+                required: "Please confirm your password",
+                validate: (value) =>
+                  value === watch("password") || "Passwords do not match",
+              })}
+            />
+          </div>
         </div>
         <div className="flex gap-4 justify-between">
           <Button type="submit" className="shrink-0" Icon={IconFingerprint}>
