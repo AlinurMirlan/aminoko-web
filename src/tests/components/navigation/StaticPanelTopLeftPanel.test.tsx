b2421531fda1a3f665aa633722a226c2f4011c94
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-04-11 11:43:24 +0600

    refactor: remove surplus comments

--- src/tests/components/navigation/StaticPanelTopLeftPanel.test.tsx
+++ src/tests/components/navigation/StaticPanelTopLeftPanel.test.tsx
@@ -1,12 +1,14 @@
-// Tests for the StaticPanelTopLeftPanel component
-
 import { render, screen } from "@testing-library/react";
 import { StaticPanelTopLeftPanel } from "../../../components/navigation/StaticPanelTopLeftPanel";
+import { store } from "../../../data/store";
+import { Provider } from "react-redux";
 
 describe("StaticPanelTopLeftPanel", () => {
   it("renders the StaticPanelTopLeftPanel component", () => {
     render(
-      <StaticPanelTopLeftPanel onClick={() => {}} />
+      <Provider store={store}>
+        <StaticPanelTopLeftPanel onClick={() => {}} />
+      </Provider>
     );
     expect(screen.getByRole("button")).toBeInTheDocument();
   });
